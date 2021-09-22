1d7231cf6467a3e0743a817069a0e04ec9e180d3
Allexiusw <william.al1379@gmail.com>
2021-09-22 00:42:51 -0600

    Add the first base test to get home page

--- api/src/core/tests.py
+++ api/src/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+# from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class MyTest(TestCase):
+    def test_welcome(self):
+        # Create your tests here.
+        client = APIClient()
+        response = client.get('/', None, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        client.logout()
