b1c03ce7356937313aa34214c6230a2b030fcf12
Allexiusw <william.al1379@gmail.com>
2021-09-23 23:38:36 -0600

    Add base model account

--- api/src/core/models.py
+++ api/src/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(_('Name'), max_length=100)
+
+    class Meta:
+        verbose_name = _("Account")
+        verbose_name_plural = _("Accounts")
+
+    def __str__(self):
+        return self.name
+
+    '''
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+    '''
